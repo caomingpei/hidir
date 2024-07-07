208ee940e8740b9df276bcca1b522315ea8e2a75
caomingpei <caomingpei@outlook.com>
2024-07-07 23:45:56 +0800

    Bump version to v0.0.1 for release

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pathlib import Path
-
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from hidir.version import VERSION
 
 
